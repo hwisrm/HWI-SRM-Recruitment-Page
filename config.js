fac8a4e2ae2c46c8bcb7d16bac59a771d38cfdf2
zealous-ideal-monk <harshil.3690@gmail.com>
2025-03-23 02:26:51 +0530

    env

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  SUPABASE_URL : 'https://zlzwhoeumogrqluulilb.supabase.co',
+  SUPABASE_KEY : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpsendob2V1bW9ncnFsdXVsaWxiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA3NjM2NDMsImV4cCI6MjA1NjMzOTY0M30.Yy6udwtoxJPZMjG1LCZeKgD5Hl0kp6l_MdPhPSIIckA'
+  };
+
+export default config;
